0dafc662dd96e43d29a12cc630038af58472453c
William Gadd <wgadd@sasktel.net>
2022-10-19 20:29:37 +0000

    Create tables for users and properties in 01_schema.sql

---
+++ migrations/01_schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS properties CASCADE;
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER REFERENCES users(id),
+  title VARCHAR(255),
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active BOOLEAN
+);
