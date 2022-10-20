74c927af3e9abaebf865061057eeaa398e539554
William Gadd <wgadd@sasktel.net>
2022-10-20 16:29:57 +0000

    Convert addUser function to add new users to the database instead of the json file

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -57,10 +57,16 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+  .query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;`, [user.name, user.email, user.password])
+  .then(data => {
+    console.log(data.rows[0])
+    return data.rows[0];
+  })
+  .catch(err => console.error(err.stack));
 }
 exports.addUser = addUser;
 
