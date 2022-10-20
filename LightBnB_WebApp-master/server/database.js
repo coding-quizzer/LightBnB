b72afc10cad3b8859e6605fc0de099616de804d7
William Gadd <wgadd@sasktel.net>
2022-10-20 16:47:36 +0000

    Convert getAllReservations function to use the sql database instead of a small json file

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -78,7 +78,17 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(
+    `SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+    FROM properties
+    JOIN reservations ON properties.id = property_id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY title, reservations.id, properties.id
+    ORDER BY start_date
+    LIMIT $2;`, [guest_id, limit])
+  .then(result => {return result.rows})
+  .catch (err => console.error(err.stack));
 }
 exports.getAllReservations = getAllReservations;
 
